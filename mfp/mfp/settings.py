ac8cc7a39258696734c764075a4c90e0536032ad
levashovn <levashovn322@gmail.com>
2018-09-22 21:32:19 +0300

    social backend changed;

--- mfp/mfp/settings.py
+++ mfp/mfp/settings.py
@@ -98,7 +98,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.github.GithubOAuth2',
     'social_core.backends.facebook.FacebookOAuth2',
-    'social.backends.linkedin.LinkedinOAuth2',
+    'social_core.backends.linkedin.LinkedinOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
